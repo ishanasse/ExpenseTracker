e0ac200b39e11a2a2c4ae3eb16bf485d441b4187
Ishan Trivedi <ishanasdeveloper@gmail.com>
2021-02-06 16:59:51 -0500

    Added Expenses model

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,13 +1,20 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from restapi import models
 
 # Create your tests here.
 
 
-def add_two(a, b):
-    return a + b
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
 
-
-class TestSum(TestCase):
-    def test_addition(self):
-        self.assertEqual(add_two(1, 2), 3)
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
